d1a548040fbed2e66a5ffe43fff96f7cb42ff3c4
karanchahal <karanbleep@gmail.com>
2018-02-03 23:48:21 +0530

    Silly errors

--- text/vocab.py
+++ text/vocab.py
@@ -1,10 +1,11 @@
 import gensim
 import nltk
+from nltk.tokenize import RegexpTokenizer
 import coloredlogs, logging
 
 # Create a logger object.
 logger = logging.getLogger(__name__)
-PROJECT_DIR = '/home/karan/attention-caption'
+PROJECT_DIR = '/home/karan/attention-caption/'
 word_model = None
 word_err_file = None
 word_errors = {}
@@ -18,9 +19,8 @@ def load_model():
 
 def write_to_log(sentence,filename):
     global word_err_file
-
-    if word_err_file is None:
-        word_err_file = open(filename, "a+")
+    logger.warning(str(sentence) + ' writing to log')
+    word_err_file = open(filename, "a+")
     word_err_file.write(sentence + '\n')
 
 def close_log():
@@ -38,9 +38,10 @@ def create(captions):
     logger.warning("Creating vocabulary")
     for batch in captions:
         for caption in batch:
-            tokens = nltk.word_tokenize(caption)
+            tokenizer = RegexpTokenizer(r'\w+')
+            tokens = tokenizer.tokenize(caption)
             for word in tokens:
                 word = word.lower()
                 if word not in word_model:
-                    collect_errors(word,filename=PROJECT_DIR + 'errors/word_error.log')
                     logger.error(str(word) + ' does not exist')
+                    collect_errors(word,filename=PROJECT_DIR + '/errors/word_error.log')
