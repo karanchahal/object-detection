fcc6481686db7630feac2bf706617c643284e71c
karanchahal <karanbleep@gmail.com>
2018-02-17 17:29:32 +0530

    updated requirements with annotations

--- dependencies.sh
+++ dependencies.sh
@@ -1,2 +1,10 @@
-echo 'All dependencies installed'
+echo 'Installing python libraries'
 pip install -r requirements.txt
+echo 'Getting annotations'
+wget http://images.cocodataset.org/annotations/annotations_trainval2014.zip
+echo 'Installing coco api'
+git clone https://github.com/cocodataset/cocoapi.git
+cd cocoapi/PythonAPI
+make
+echo 'Installed all dependencies'
+
