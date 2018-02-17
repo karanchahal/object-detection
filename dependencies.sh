ba10d36e437a84e7c9d66f3eccd98c6e88834b79
karanchahal <karanbleep@gmail.com>
2018-02-17 17:35:50 +0530

    make in place

--- dependencies.sh
+++ dependencies.sh
@@ -5,6 +5,6 @@ wget http://images.cocodataset.org/annotations/annotations_trainval2014.zip
 echo 'Installing coco api'
 git clone https://github.com/cocodataset/cocoapi.git
 cd cocoapi/PythonAPI
-make
+make all
 echo 'Installed all dependencies'
 
