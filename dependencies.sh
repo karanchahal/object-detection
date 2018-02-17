b83eb12dc4aebc4c2995a891afb1fa98b925542e
karanchahal <karanbleep@gmail.com>
2018-02-17 18:24:01 +0530

    unzip errors

--- dependencies.sh
+++ dependencies.sh
@@ -1,9 +1,9 @@
 echo 'Getting annotations'
 wget http://images.cocodataset.org/annotations/annotations_trainval2014.zip
-unzip annotations_trainval2014.zip
+unzip annotations_trainval2014.zip -y
 echo 'Getting word embeddings'
 wget http://nlpserver2.inf.ufrgs.br/alexandres/vectors/lexvec.enwiki%2bnewscrawl.300d.W.pos.vectors.gz
-unzip lexvec.enwiki+newscrawl.300d.W.pos.vectors.gz
+unzip lexvec.enwiki+newscrawl.300d.W.pos.vectors.gz -y
 echo 'Installing coco api'
 git clone https://github.com/cocodataset/cocoapi.git
 cd cocoapi/PythonAPI
