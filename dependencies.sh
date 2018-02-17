39486b3622309265a5de38e26633312702bb11f4
karanchahal <karanbleep@gmail.com>
2018-02-17 17:39:30 +0530

    pycocotools debug

--- dependencies.sh
+++ dependencies.sh
@@ -1,10 +1,10 @@
-echo 'Installing python libraries'
-pip install -r requirements.txt
 echo 'Getting annotations'
 wget http://images.cocodataset.org/annotations/annotations_trainval2014.zip
 echo 'Installing coco api'
 git clone https://github.com/cocodataset/cocoapi.git
 cd cocoapi/PythonAPI
-make all
+make
+echo 'Installing python libraries'
+pip install -r requirements.txt
 echo 'Installed all dependencies'
 
