1b11802f87306912ff1d60e92c64a4cc3f0600a5
Karanbir Chahal <karanbleep@gmail.com>
2018-07-05 11:13:56 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,13 @@
 #Todo
 echo 'All tests passed'
+
+git clone https://github.com/cocodataset/cocoapi.git
+cd cocoapi/PythonAPI
+make
+make install
+python setup.py install
+cd ../.././
+ls
+echo 'Some file folder modifications'
+mkdir errors
+echo 'Installed all dependencies'
