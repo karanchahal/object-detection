c43b9056d08bac5bc86fe3492ee71ebff6d16de0
Karanbir Chahal <karanbleep@gmail.com>
2018-07-05 11:49:16 +0530

    Update dependencies.sh
    Word vectors reference removed. Server no longer is up

--- dependencies.sh
+++ dependencies.sh
@@ -1,10 +1,10 @@
 echo 'Getting annotations'
 wget http://images.cocodataset.org/annotations/annotations_trainval2014.zip
 unzip annotations_trainval2014.zip
-echo 'Getting word embeddings'
-wget http://nlpserver2.inf.ufrgs.br/alexandres/vectors/lexvec.enwiki%2bnewscrawl.300d.W.pos.vectors.gz
-gunzip -k lexvec.enwiki+newscrawl.300d.W.pos.vectors.gz
-mv ./lexvec.enwiki+newscrawl.300d.W.pos.vectors ./lex.vectors
+#echo 'Getting word embeddings'
+#wget http://nlpserver2.inf.ufrgs.br/alexandres/vectors/lexvec.enwiki%2bnewscrawl.300d.W.pos.vectors.gz
+#gunzip -k lexvec.enwiki+newscrawl.300d.W.pos.vectors.gz
+#mv ./lexvec.enwiki+newscrawl.300d.W.pos.vectors ./lex.vectors
 echo 'Installing python libraries'
 pip install -r requirements.txt
 echo 'Installing coco api'
