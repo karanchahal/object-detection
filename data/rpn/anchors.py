0cd276b854b5bafc5982d881f681fd3ac618adc1
karanchahal <karanbleep@gmail.com>
2018-07-09 18:52:08 +0530

    Functions comments

--- data/rpn/anchors.py
+++ data/rpn/anchors.py
@@ -1,8 +1,26 @@
 def getAnchorsForPixelPoint(i,j,width,height):
-    ''' Generates 9 anchors for a single pixel point. Each anchor has 4 coordinates.
+    ''' 
+     Generates 9 anchors for a single pixel point. Each anchor has 4 coordinates.
      The center x,y, coordinate and the height and the width. These 9 anchors are generated
      by taking 2 aspect ratios and 3 scales.
+
+    Arguments:
+    i: (int) . center x coordinate of anchor 
+    j: (int) - center y coordinate of anchor 
+    width: (int) . Width of the image
+    height: (int) - Height of the image
+
+    Returns:
+    anchors: [ [int,int,int,int] ] An list of lists. Each interior list has 4 ints.
+    the [int,int,int,int] is => [Center x, Center y, Height, Width]
+
      ''' 
+
+    assert(isinstance(i, int)),"i coordinate should be of type int"
+    assert(isinstance(j),int),"j coordinate should be of type int"
+    assert(isinstance(width, int)),"Width should be of type int"
+    assert(isinstance(height),int),"Height should be of type int"
+
     anchors = [] 
     scales = [64,128,256]
     aspect_ratios = [ [1,1] ]
@@ -21,7 +39,8 @@ def getAnchorsForPixelPoint(i,j,width,height):
 
 
 def generateAnchors(width,height,compressionFactor):
-    ''' This function return a list of anchors for an image of some height and width.
+    ''' 
+    This function return a list of anchors for an image of some height and width.
     With a compression factor of c. That means anchors for two different center pixels
     are seperated by c pixels  in an image
     
