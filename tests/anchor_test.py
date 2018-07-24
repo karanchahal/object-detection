d2ba17855651059046f7608744dc129995a651a2
karanchahal <karanbleep@gmail.com>
2018-07-24 23:45:04 +0530

    Init tests

---
+++ tests/anchor_test.py
@@ -0,0 +1,20 @@
+import unittest
+
+class TestAnchors(unittest.TestCase):
+
+    def test_iou(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_offsets(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_anchors(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+if __name__ == '__main__':
+    unittest.main()
